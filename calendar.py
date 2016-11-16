27b53e80857a0709c1e93d1db78149a7cf4de2a8
Alvin <alvinmutisya@gmail.com>
2016-11-16 15:11:29 +0300

    Added more functionality to the method add_event

--- calendar.py
+++ calendar.py
@@ -16,18 +16,25 @@ class Calendar(object):
     def add_event(self, event_date, event_month, event_day, event_name):
         """
             Function makes use of the calendar to book various dates to event.
-            :param date:
-            :param month:
-            :param day:
-            :param event:
-            :return dictionary{"date": "event"}
+            :param event_date:
+            :param event_month:
+            :param event_day:
+            :param event_name:
+            :return: dictionary{"date": "event"}
         """
 
-        self.event_date = event_date
-        self.event_month = event_month
         self.event_day = event_day
+        self.event_month = event_month
+        self.event_desc = event_desc
         self.event_name = event_name
-
+        event = [event_name, event_desc]
+        date = str(self.event_month) + str(self.event_day)
+        if self.event_day in self._days:
+            for days in self._days:
+                self._calender_data[days] = event
+            return self._calender_data[days] = event
+        else:
+            return "Sorry date already booked, view events"
 
     def view_events(self):
         pass
