da42fdcd0a40fcf134ae7f423c2d27fa335726a9
GathuBoswell <gathudev@gmail.com>
2016-11-16 14:42:56 +0300

    added the create calendar functionality

--- calendar.py
+++ calendar.py
@@ -5,10 +5,13 @@ class Calendar(object):
     """
     def __init__(self):
         self._calender_data = {}
+        self._days = []
 
-    def create_calendar(self):
-        pass
-
+    def create_calendar(self, calendar_month, calendar_year):
+        import calendar
+        calendar1 = [item for item in calendar.month(calendar_year, calendar_month).split()]
+        for item in calendar1[9:]:
+            self._days.append(int(item))
 
     def add_event(self):
         pass
