195ad4e25180f87fd6e9572c1113f977f587ab67
angieMutava <mutavaangela@yahoo.com>
2016-11-17 01:21:19 -0800

    Initial commit

---
+++ calendar.py
@@ -0,0 +1,21 @@
+class Calendar(object):
+	"""
+    This is a simple calendar application the enables users to
+     create calendar,adds events, views events
+	"""
+
+	def create_calendar(self):
+		pass
+		
+
+    def add_event(self):
+        pass
+
+
+    def view_events(self):
+        pass
+
+
+    def view_last_event(self):
+        pass      		
+
