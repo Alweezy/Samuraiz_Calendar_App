ca0c98d8164116c0d38bdccecdac3d33caf60902
GathuBoswell <gathudev@gmail.com>
2016-11-17 18:30:27 +0300

    removed the excess new lines prints

--- calendarcli.py
+++ calendarcli.py
@@ -24,7 +24,6 @@ def main():
 	while True:
 		print(" 1. Create calendar\n 2. Add Event\n 3. View Events \n 4. View Last Entry \n 5. Exit \n \n")
 		action = input('Enter your choice: ')
-		print('\n')
 		if action.lower() == '1':
 			Mycal = Calendar()
 			month = input('Enter month: ')
@@ -33,16 +32,12 @@ def main():
 			print('\n')
 		elif action.lower() == '2':
 			event_day = input("Add Event day: ")
-			print('\n')
 			event_name = input("Add Event name: ")
-			print('\n')
 			event_desc = input("Add Event description: ")
-			print('\n')
 			print (Mycal.add_event(event_day, event_name, event_desc))
 		elif action.lower() == '3':
 			print('\n')
 			print (Mycal.view_events())
-			print('\n')
 		elif action.lower() == '4':
 			print('\n')
 			print (Mycal.view_last_event())
