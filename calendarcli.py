d0106c3b6018e44b23d2e03b8e4b8b94bc04e761
GathuBoswell <gathudev@gmail.com>
2016-11-18 10:39:50 +0300

    removed merge conflict markers

--- calendarcli.py
+++ calendarcli.py
@@ -19,42 +19,7 @@ def main():
 		calendar create
 
 	"""
-<<<<<<< HEAD
-	print("\n\n")
-	print ("Welcome to your Personal Calendar\n\n")
-
-	while True:
-		print(" 1. Create calendar\n 2. Add Event\n 3. View Events \n 4. View Last Entry \n 5. Exit \n \n")
-		action = input('Enter your choice: ')
-		print('\n')
-		if action.lower() == 1:
-			Mycal = Calendar()
-			month = input('Enter month: ')
-			year = input('Enter year: ')
-			Mycal.create_calendar(int(year), int(month))
-			print('\n')
-		elif action.lower() == '2':
-			event_day = input("Add Event day: ")
-			print('\n')
-			event_name = input("Add Event name: ")
-			print('\n')
-			event_desc = input("Add Event description: ")
-			print('\n')
-			print (Mycal.add_event(event_day, event_name, event_desc))
-		elif action.lower() == '3':
-			print('\n')
-			print (Mycal.view_events())
-			print('\n')
-		elif action.lower() == '4':
-			print('\n')
-			print (Mycal.view_last_event())
-			print('\n')
-		elif action.lower() == '5':
-			break
-		else:
-			print ("Invalid choice: select again")
 			
-=======
     print("\n\n")
     print("Welcome to your Personal Calendar\n\n")
 
@@ -63,11 +28,11 @@ def main():
         action = input('Enter your choice: ')
         if action == '1':
         	calendar_name = input('Enter your calendar name: ')
-            month = input("Enter month e.g 7 for 'July': ")
-            year = input('Enter year e.g 2016: ')
-            calendar_name = Calendar()
-            calendar_name.create_calendar(int(year), int(month))
-            print('\n')
+            	month = input("Enter month e.g 7 for 'July': ")
+            	year = input('Enter year e.g 2016: ')
+            	calendar_name = Calendar()
+            	calendar_name.create_calendar(int(year), int(month))
+           	print('\n')
         	print('Calendar created.')
         	print('\n')
         elif action == '2':
@@ -100,7 +65,5 @@ def main():
             break
         else:
             print("Invalid choice: select again")
-
->>>>>>> 1b3eac44874381e00171baa9da61c25892d61c47
 
 if __name__ == '__main__': main()
